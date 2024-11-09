740758ea50c139c84ccb0e942dab0db0373efc9e
Jon Lake <jonthemonke@users.noreply.github.com>
2024-11-08 18:29:03 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::env;
+
 use rocket::launch;
 use rocket::fs::FileServer;
+use rocket::config::Config;
 
 const PATH: &str = "$HOME/.config/arch-jlake-co/static";
 
 #[launch]
 fn rocket() -> _ {
+
+    let port = env::var("PORT").unwrap_or("80".to_string());
+    let port = port.parse::<u16>().unwrap();
+    let mut config = Config::default();
+    config.port = port;
+
     let home: String = env::var("HOME").unwrap();
     let path = PATH.replace("$HOME", &home);
-    rocket::build().mount("/", FileServer::from(path))
+
+    rocket::custom(config).mount("/", FileServer::from(path))
 }
